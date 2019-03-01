f4122a87cacdfb2b6ebee5d02759f6eb589cf271
李常春 <lichangchun@jinyiyun.com.cn>
2019-03-01 14:28:26 +0800

    fix: 调整仅显示字段的样式

--- resources/views/form/display.blade.php
+++ resources/views/form/display.blade.php
@@ -1,9 +1,9 @@
 <div class="{{$viewClass['form-group']}}">
     <label class="{{$viewClass['label']}} control-label">{{$label}}</label>
     <div class="{{$viewClass['field']}}">
-        <div class="box box-solid box-default no-margin">
+        <div class="box box-solid no-margin">
             <!-- /.box-header -->
-            <div class="box-body">
+            <div class="box-body" style="background-color: #F7F7F7;">
                 {!! $value !!}&nbsp;
             </div><!-- /.box-body -->
         </div>
