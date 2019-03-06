71bbd95ded4af0bdf38f4869aaecf955df78a64a
李常春 <lichangchun@jinyiyun.com.cn>
2019-03-06 13:49:08 +0800

    fix: MAP控件经纬度传值错误

--- resources/views/form/map.blade.php
+++ resources/views/form/map.blade.php
@@ -7,8 +7,11 @@
         @include('admin::form.error')
 
         <div id="map_{{$id['lat'].$id['lng']}}" style="width: 100%;height: 300px"></div>
-        <input type="hidden" id="{{$id['lat']}}" name="{{$name['lat']}}" value="{{ old($column['lat'], $value['lat']) }}" {!! $attributes !!} />
-        <input type="hidden" id="{{$id['lng']}}" name="{{$name['lng']}}" value="{{ old($column['lng'], $value['lng']) }}" {!! $attributes !!} />
+        <!-- <input type="hidden" id="{{$id['lat']}}" name="{{$name['lat']}}" value="{{ old($column['lat'], $value['lat']) }}" {!! $attributes !!} />
+        <input type="hidden" id="{{$id['lng']}}" name="{{$name['lng']}}" value="{{ old($column['lng'], $value['lng']) }}" {!! $attributes !!} /> -->
+
+        <input type="hidden" id="{{$id['lat']}}" name="{{$name['lat']}}" value="{{ $column['lat'] }}" {!! $attributes !!} />
+        <input type="hidden" id="{{$id['lng']}}" name="{{$name['lng']}}" value="{{ $column['lng'] }}" {!! $attributes !!} />
 
         @include('admin::form.help-block')
 
